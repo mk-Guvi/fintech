3ea2f7b5016d53e44991d285aeba7f54e030c260
Musharaf <newuser@News-MacBook-Air.local>
2024-09-16 21:02:59 +0530

    [FIX] added index file for server

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,5 @@
 const { createRequestHandler } = require('@expo/server/adapter/vercel');
-const path = require('path');
 
 module.exports = createRequestHandler({
-  build: path.join(__dirname, '../dist/server') // Adjust this to point to your built server files
+  build: require('path').join(__dirname, '../dist/server'),
 });
